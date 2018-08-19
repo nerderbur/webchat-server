a554e496184c469bcb822e693a3f75778a59222a
Unknown <wrightleon@icloud.com>
2018-08-19 17:52:17 -0500

    Changed starting port to 3000

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-server.listen(80);
+server.listen(3000);
 
 app.get('/', (req, res) => {
-    res.send('Hello world!');
+    res.send('Hello world');
 })
 
 io.on('connection', (socket) => {
