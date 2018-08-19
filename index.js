73514a5917ed262d06b84222dd8bf4cf7a8e9086
Unknown <wrightleon@icloud.com>
2018-08-19 17:06:00 -0500

    Server config

---
+++ index.js
@@ -0,0 +1,17 @@
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+server.listen(80);
+
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+})
+
+io.on('connection', (socket) => {
+    console.log('Client connected!')
+    socket.emit('news', {hello: 'world'});
+    socket.on('input', (data) => {
+        console.log(data)
+    })
+})
